70d55d3cf43c608089284b754cd64c639cb7c2b7
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-24 13:49:56 +0200

    style: style details page and add shadcn badge

--- src/pages/module/[uid].tsx
+++ src/pages/module/[uid].tsx
@@ -1,7 +1,10 @@
 import type { GetServerSideProps } from "next";
-import React from "react";
 import APIClient from "~/lib/services/api-client";
 import type { Module } from "~/lib/types";
+import Image from "next/image";
+import Link from "next/link";
+import { Badge } from "~/components/ui/badge";
+import { Button } from "~/components/ui/button";
 
 const apiClient = new APIClient("/catalog");
 
@@ -25,9 +28,46 @@ interface Props {
 const DetailsRoute = ({ moduleDetails }: Props) => {
   return (
     <>
-      <div>{moduleDetails.title}</div>
-      <div>{moduleDetails.summary}</div>
-      <div>{moduleDetails.url}</div>
+      <div className="mt-12 flex min-h-screen flex-col gap-12 text-foreground">
+        <div className="relative flex w-full flex-col items-center gap-6 sm:flex-row">
+          <Image
+            src={moduleDetails.icon_url}
+            width={250}
+            height={250}
+            alt={moduleDetails.title}
+          />
+          <div className="bottom-0 flex flex-col items-center justify-center gap-5 p-4 sm:items-start">
+            <h1 className="items-center text-center text-2xl font-bold sm:text-left sm:text-4xl">
+              {moduleDetails.title}
+            </h1>
+            <div className="flex flex-col items-center gap-4 sm:items-start">
+              <div>
+                <span className="font-bold">
+                  {moduleDetails.rating.average}/5
+                </span>
+                {" - "}({moduleDetails.rating.count})
+              </div>
+
+              {moduleDetails.subjects && (
+                <Badge className="self-start p-2 px-4">
+                  {moduleDetails.subjects}
+                </Badge>
+              )}
+            </div>
+          </div>
+        </div>
+        <section>
+          <h2 className="text-xl font-bold sm:text-3xl">Description:</h2>
+          <p>{moduleDetails.summary}</p>
+          <div>
+            <Link href={moduleDetails.url} target="_blank">
+              <Button variant="outline" className="mt-4">
+                Go To Lesson
+              </Button>
+            </Link>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
