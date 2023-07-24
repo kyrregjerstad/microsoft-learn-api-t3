2f43162fc4579b7456f69c7562aba33e7e2d74a2
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-24 12:42:02 +0200

    add dependencies and remove boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
